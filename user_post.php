8c578d13e0d903969949d8c285d651ddd56c777f
fadh0003 <fadh0003@stud.kea.dk>
2018-05-11 14:46:14 +0200

    testing

--- user_post.php
+++ user_post.php
@@ -37,27 +37,29 @@ if(mysqli_connect_errno())
             $imageType = $_FILES['postimage']['type'];
     
             $fileExt = explode('.', $imageName); //image . jpg
-    
-            print_r($imageName);
-    
+
             $fileAcualExt = strtolower(end($fileExt)); //jpg
+
+            print_r($imageName);
+            echo "</br>";
+            print_r($fileAcualExt);
     
             $allowed = array('jpg', 'jpeg');
             //checks if $fileAcualExt is inside $allowed array
             if(in_array($fileAcualExt, $allowed)){
                 //if file is allowed and have no error
                 if($imageError === 0){
-                    $newImageName = uniqid() . "_" . $fileAcualExt;
+                    //$newImageName = uniqid() . "_" . $fileAcualExt;
                     /*$fileDestination = 'UploadImages/'.$newImageName;
                     move_uploaded_file($imageTmpName, $fileDestination);*/
 
                     //select userid query
-                $useridquery =  mysqli_query($DB_connection, "SELECT user_id FROM users WHERE user_name = '$post_username'") or die('ERROR: SQL query problem' . mysqli_error($DB_connection));
-                $row = mysqli_fetch_assoc($useridquery);
-                $userid = $row['user_id'];
+                //$useridquery =  mysqli_query($DB_connection, "SELECT user_id FROM users WHERE user_name = '$post_username'") or die('ERROR: SQL query problem' . mysqli_error($DB_connection));
+                //$row = mysqli_fetch_assoc($useridquery);
+                //$userid = $row['user_id'];
                 //insert post query
-                $postsquery = mysqli_query($DB_connection, "INSERT INTO posts (post_text, post_image, post_date, fk_user_id) VALUES('$post_text', '$newImageName', CURRENT_TIMESTAMP, $userid)") or die('ERROR: SQL query problem' . mysqli_error($DB_connection));
-                $querystring = "INSERT INTO posts (post_text, post_image, post_date, fk_user_id) VALUES('$post_text', '30590797_946679478834376_6124683747041214464_n.jpg', CURRENT_TIMESTAMP, $userid)";
+                //$postsquery = mysqli_query($DB_connection, "INSERT INTO posts (post_text, post_image, post_date, fk_user_id) VALUES('$post_text', '$newImageName', CURRENT_TIMESTAMP, $userid)") or die('ERROR: SQL query problem' . mysqli_error($DB_connection));
+                //$querystring = "INSERT INTO posts (post_text, post_image, post_date, fk_user_id) VALUES('$post_text', '30590797_946679478834376_6124683747041214464_n.jpg', CURRENT_TIMESTAMP, $userid)";
                 //echo $querystring;
 
 

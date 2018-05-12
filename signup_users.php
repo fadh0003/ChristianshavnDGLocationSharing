1d831af5da4f657d6bf9f4a02a8c1c7e1822efb3
fadh0003 <fadh0003@stud.kea.dk>
2018-05-12 10:21:40 +0200

    email validation

--- signup_users.php
+++ signup_users.php
@@ -22,13 +22,21 @@ if($_POST){
 
     $namecheckquery = "SELECT user_name FROM users WHERE user_name = '" .$username. "';";
     $namecheck = mysqli_query($DB_connection, $namecheckquery) or die('ERROR: SQL query problem' . mysqli_error($DB_connection));
-    $row = mysqli_fetch_assoc($namecheck); 
+    $rowusername = mysqli_fetch_assoc($namecheck); 
 
-    if($row['user_name'] == $username){
+    if($rowusername['user_name'] == $username){
         echo "3: Name already exsits";
         exit();
-    } else {
+    } 
     
+    $emailcheckquery = "SELECT user_email FROM users WHERE user_email = '" .$email. "';";
+    $emailcheck = mysqli_query($DB_connection, $emailcheckquery) or die('ERROR: SQL query problem' . mysqli_error($DB_connection));
+    $rowuseremail = mysqli_fetch_assoc($emailcheck); 
+    
+    if($rowuseremail['user_email'] == $email){
+        echo "4: Email already exsits";
+        exit();
+    } else {
     $passwordhash = password_hash($password, PASSWORD_DEFAULT);    
     mysqli_query($DB_connection, "INSERT INTO users(user_name, user_email, user_password, fk_role_id) VALUES('$username','$email', '$passwordhash', '2')") or die ('ERROR: SQL query problem' . mysqli_error($DB_connection));
     echo("0");

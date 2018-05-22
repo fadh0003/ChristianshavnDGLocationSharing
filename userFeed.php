6cceb6c80a03f64f06581370c345bc07bd450653
fadh0003 <fadh0003@stud.kea.dk>
2018-05-22 18:25:52 +0200

    test2

--- userFeed.php
+++ userFeed.php
@@ -16,11 +16,12 @@ if(mysqli_connect_errno())
     $userpostquery = "SELECT * FROM posts INNER JOIN users on posts.fk_user_id=users.user_id";
     $fetchuserpost = mysqli_query($DB_connection, $userpostquery);
     
-    //$row = mysqli_fetch_assoc($fetchuserpost); 
-    while($row = mysqli_fetch_assoc($fetchuserpost)){ 
-        
+    $row = mysqli_fetch_assoc($fetchuserpost);
+    echo $row['post_image'];
+     
+    /*while($row = mysqli_fetch_assoc($fetchuserpost)){ 
         echo $row['user_name'];
         echo $row['post_image'];
         echo $row['post_text'];
-    }
+    }*/
 ?>

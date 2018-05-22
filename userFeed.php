383abc3a145b1018684d4cc03fd6b05bf62b94c8
fadh0003 <fadh0003@stud.kea.dk>
2018-05-22 18:15:17 +0200

    test

---
+++ userFeed.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+include('DBconnection.php');
+
+//include('wideimage/WideImage.php');
+
+
+//checking connection
+if(mysqli_connect_errno())
+{
+    echo "1: Connection failed";
+    exit();
+}
+/***************/
+    $userpostquery = "SELECT * FROM posts INNER JOIN users on posts.fk_user_id=users.user_id";
+    $fetchuserpost = mysqli_query($DB_connection, $userpostquery);
+    
+    //$row = mysqli_fetch_assoc($fetchuserpost); 
+    while($row = mysqli_fetch_assoc($fetchuserpost)){ 
+        
+        echo $row['user_name']. " " .$row['post_image']. " " .$row['post_text'];
+        echo " ";
+    }
+?>

9902d3c972b0ccd7695f28b1596079de5a047b9f
fadh0003 <fadh0003@stud.kea.dk>
2018-06-10 13:18:19 +0200

    login test5

--- signin_users.php
+++ signin_users.php
@@ -35,7 +35,7 @@ if($_POST){
         $validateUsername = true;
     } else {
         $validateUsername = false;
-        echo "incorrectUsername";
+        echo "incorrectUsername ";
     }
     
     if(password_verify($password, $matchUserPassword)){
@@ -47,16 +47,13 @@ if($_POST){
     }
     else {
         $validatePassword = false;
-        echo "incorrectPassword";
+        echo "incorrectPassword ";
     }
 
     if($validateUsername && $validatePassword){
         if($fetchUserTypeLogin['fk_role_id'] == 2){
             echo "0";
         }
-    }
-    else {
-        echo "Login information does not exist";
     }
 }
 
